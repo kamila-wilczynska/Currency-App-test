1dfd5084a09acb2aa10b75213faed648cffa5a3e
unknown <kamila.wilczynska92@gmail.com>
2023-04-23 13:23:40 +0100

    function fixed and change for .toBeNan

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -7,5 +7,17 @@ export const convertPLNToUSD = (PLN) => {
     currency: 'USD'
   });
 
+  if (typeof PLN === 'string' || typeof PLN === 'undefined') {
+    return NaN;
+  }
+  
+  if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 }
